f82de5a1fe69cb3f3c3cd14a0ff6d5a6655ef87a
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2023-01-30 04:05:15 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+# ______   _   _  
+# | ___ | | | / / C-Keenan
+# | | |_| | |/ /  https://github.com/C-Keenan
+# | |  _  |   /   
+# | |_| | |   \   
+# |_____| |_|\_\  
+
+# test script for learning purposes
+
+ls
+
