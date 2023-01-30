e4b88e2163e5b7ba57d481b45b6a91c66f921ea0
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2023-01-30 04:23:20 -0500

    create refreshtest.sh

---
+++ refreshtest.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+# ______   _   _  
+# | ___ | | | / / C-Keenan
+# | | |_| | |/ /  https://github.com/C-Keenan
+# | |  _  |   /   
+# | |_| | |   \   
+# |_____| |_|\_\  
+
+# test script for learning purposes changes often and should be discarded, not run
+
+cp test.sh.1 test.sh
+cat test.sh
+sleep 20
+rm test.sh.1
