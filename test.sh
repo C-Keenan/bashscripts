8eb1bd682e9bae235d6f6a75de67a8fad7583579
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2023-01-31 08:44:46 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -16,3 +16,19 @@ root="/"
 lanc="/mnt/lancache/lancache"
 docker="/home/cameron/docker"
 libspd="/home/cameron/docker/librespeedtest"
+iscsidsc="sudo iscsiadm -m discovery -t sendtargets -p 192.168.1.2"
+iscsimnt="sudo iscsiadm --mode node --targetname iqn.2011-08.com.asustor:as1004t-8dc32d.lancache --portal 192.168.1.2 && sudo mount /dev/sdd1 /mnt/lancache"
+reboot="sudo reboot"
+iscsilogout="sudo iscsiadm -m node -u"
+iscsidmnt="sudo umount /dev/sdd1"
+
+# script start
+echo "Some commands in this script will need sudo elevation. Please provide 'sudo' password when promted."
+echo ""
+echo ""
+cd $home
+cd $lanc
+$dcstop
+cd $home
+$iscsidmnt
+$iscsilogout
