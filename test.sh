239a36299dfdb0bb3f15a3c04df158dedcdd603c
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2023-01-31 09:04:43 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ lanc="/mnt/lancache/lancache"
 docker="/home/cameron/docker"
 libspd="/home/cameron/docker/librespeedtest"
 iscsidsc="sudo iscsiadm -m discovery -t sendtargets -p 192.168.1.2"
-iscsimnt="sudo iscsiadm --mode node --targetname iqn.2011-08.com.asustor:as1004t-8dc32d.lancache --portal 192.168.1.2 && sudo mount /dev/sdd1 /mnt/lancache"
+iscsimnt="sudo iscsiadm --mode node --targetname iqn.2011-08.com.asustor:as1004t-8dc32d.lancache --portal 192.168.1.2 --login && sudo mount /dev/sdd1 /mnt/lancache"
 reboot="sudo reboot"
 iscsilogout="sudo iscsiadm -m node -u"
 iscsidmnt="sudo umount /dev/sdd1"
@@ -28,7 +28,9 @@ echo ""
 echo ""
 cd $home
 $iscsidsc
+sleep 10
 $iscsimnt
+sleep 10
 cd $lanc
 $dcstart
 cd $home
